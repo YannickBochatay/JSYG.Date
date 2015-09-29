e10021afdc3c0c9196189a282b45cc3521785768
bochatayy <yannick.bochatay@meteo.fr>
2015-09-29 08:44:24 +0200

    Commit initial.

---
+++ tests/tests.js
@@ -0,0 +1,37 @@
+if (typeof require!= "undefined") {
+    
+    require.config({
+        paths: {
+            "date": '../JSYG.Date'
+        },
+        urlArgs: "bust=" + (+new Date())
+    });
+}
+
+(function(factory) {
+    
+    if (typeof define == 'function' && define.amd) define(["date"],factory);
+    else factory(JDate);
+    
+}(function(JDate) {
+
+    module("Date");
+
+    test("Manipulation d'une date", function() {     
+        
+        var date = new JDate();
+        
+        date.set("month","01");
+        date.set("day","01");
+        date.set("year","2015");
+        
+        expect(4);
+        
+        equal( date.get("month"), 1, "récupération du mois");
+        equal( date.get("year"), 2015, "récupération de l'année");
+        
+        equal( date.add("1 month").get("month") , 2, "ajout d'un mois");
+        equal( date.add("-1 day").get("month"), 1, "retrait d'un jour");
+    });
+    
+}));
